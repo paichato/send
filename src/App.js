e121796fb9c2b4fa80d69699cdc4103075d63515
Marlon Strong <tsenane14@gmail.com>
2021-01-22 02:02:35 +0200

    added styles

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
-
+import Home from './Pages/Home';
+import './App.css';
 
 
 function App() {
   return (
     <div className="App">
-      
+      <Home/>
     </div>
   );
 }
--- src/Components/HeroElements.js
+++ src/Components/HeroElements.js
@@ -1,20 +1,98 @@
 import styled from 'styled-components';
 
 export const Nav=styled.nav`
+display:flex;
+/* position: sticky; */
+justify-content:space-between;
+align-items:center;
+height:80px;
+padding: 0rem calc((100vw - 1300px)/2);
+background:#FFBF00;
 
 
 `
 
 export const Logo=styled.div`
-
+font-size:1.5rem;
+color:#251101;
+font-weight:bold;
+font-style: italic;
+padding: 0 2rem;
 
 `
 
 export const NavItems=styled.div`
 
-a{
 
+
+a{
+    text-decoration:none;
+    color:#251101;
+    padding: 0 1.5rem;
+    
 }
 
+`
+export const HeroSection=styled.section`
+background:#FFBF00;
+
+`
+export const HeroContainer=styled.div`
+/* display:flex;
+flex-direction:row; */
+
+display:grid;
+grid-template-columns: 1fr 1fr;
+height:95vh;
+padding: 0rem calc((100vw - 1300px)/2);
+`
+export const HeroLeft=styled.div`
+display:flex;
+flex-direction:column;
+align-items:flex-start;
+padding: 0 2rem;
+
+h1{
+    font-size: 3rem;
+    margin-bottom:1rem;
+}
+
+p{
+line-height:1.5rem;
+font-size:1.5rem;
+margin-bottom:2rem;
+}
+
+`
+export const HeroButtonWrapper=styled.div`
+
+button{
+        border-radius:100px;
+        color: white;
+        background:#251101;
+        padding: 1rem 3rem;
+        border:none;
+        outline:none;
+        font-weight:bold;
+        font-size:1rem;
+        cursor: pointer;
+
+        &:hover{
+            background:#140901;
+        }
+}
+
+`
+export const HeroBgContainer=styled.div`
+display:flex;
+justify-content:center;
+align-items:center;
+padding: 0 2rem;
+`
+
+export const HeroBg=styled.img`
+
+width:100%;
+height:100%;
 `
 
